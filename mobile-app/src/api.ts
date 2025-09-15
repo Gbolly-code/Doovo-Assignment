04b59bf9de49c5693ce32e408d71f254442313db
olaniyan oluwagbolahan <olaniyangbolahan01@gmail.com>
2025-09-15 20:17:54 +0100

    added the deployed backend

--- mobile-app/src/api.ts
+++ mobile-app/src/api.ts
@@ -1,12 +1,12 @@
-// src/api.ts
+
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const api = axios.create({
-  baseURL: 'http://172.20.10.3:4000',
+  baseURL: 'https://doovo-assignment-production.up.railway.app/',
 });
 
-// attach JWT automatically
+
 api.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem("token");
   if (token) {
