d1db0a6c7b41cc781914caf9bef62f69daf45d17
olaniyan oluwagbolahan <olaniyangbolahan01@gmail.com>
2025-09-16 00:23:26 +0100

    use env PORT

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
    app.useGlobalFilters(new AllExceptionsFilter());
     
    const port = process.env.PORT || 4000;
-  await app.listen(port);
+  await app.listen(port, '0,0,0,0');
 
   console.log(`Server running on port ${port}`);
 }
